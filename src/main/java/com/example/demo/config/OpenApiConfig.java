5d6741e5b0f94c774c8eb4223c18dd97f6d810ef
bala-amypo <bala@amypo.in>
2025-12-26 10:32:44 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-/*
+
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.Components;
@@ -42,7 +42,6 @@ public class OpenApiConfig {
 }
 
 
-*/
 
 
 // package com.example.demo.config;
@@ -85,31 +84,31 @@ public class OpenApiConfig {
 
 
 
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.security.SecurityRequirement;
+// import io.swagger.v3.oas.models.security.SecurityScheme;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class OpenApiConfig {
+// @Configuration
+// public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Influencer Campaign ROI Tracker API")
-                        .version("1.0")
-                        .description("API for tracking influencer campaign ROI"))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
-}
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Influencer Campaign ROI Tracker API")
+//                         .version("1.0")
+//                         .description("API for tracking influencer campaign ROI"))
+//                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+//                 .components(new io.swagger.v3.oas.models.Components()
+//                         .addSecuritySchemes("Bearer Authentication",
+//                                 new SecurityScheme()
+//                                         .type(SecurityScheme.Type.HTTP)
+//                                         .scheme("bearer")
+//                                         .bearerFormat("JWT")));
+//     }
+// }
