2f4004bcd3b15ef0517c896fb2097dbd88e16c26
bala-amypo <bala@amypo.in>
2025-12-28 17:54:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/InfluencerController.java
+++ src/main/java/com/example/demo/controller/InfluencerController.java
@@ -36,41 +36,9 @@
 //     }
 // }
 
-// package com.example.demo.controller;
 
-// import com.example.demo.model.Influencer;
-// import com.example.demo.service.InfluencerService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
 
-// @RestController
-// @RequestMapping("/influencers")
-// public class InfluencerController {
 
-//     private final InfluencerService influencerService;
-
-//     @Autowired
-//     public InfluencerController(InfluencerService influencerService) {
-//         this.influencerService = influencerService;
-//     }
-
-//     @PostMapping
-//     public ResponseEntity<Influencer> createInfluencer(@RequestBody Influencer influencer) {
-//         return ResponseEntity.ok(influencerService.createInfluencer(influencer));
-//     }
-
-//     @GetMapping
-//     public ResponseEntity<List<Influencer>> getAllInfluencers() {
-//         return ResponseEntity.ok(influencerService.getAllInfluencers());
-//     }
-
-//     @GetMapping("/{id}")
-//     public ResponseEntity<Influencer> getInfluencer(@PathVariable Long id) {
-//         return ResponseEntity.ok(influencerService.getInfluencerById(id));
-//     }
-// }
 
 package com.example.demo.controller;
 
--- src/main/java/com/example/demo/controller/RoiReportController.java
+++ src/main/java/com/example/demo/controller/RoiReportController.java
@@ -37,41 +37,7 @@
 // 
 
 
-// package com.example.demo.controller;
 
-// import com.example.demo.model.RoiReport;
-// import com.example.demo.service.RoiService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/roi")
-// public class RoiReportController {
-
-//     private final RoiService roiService;
-
-//     @Autowired
-//     public RoiReportController(RoiService roiService) {
-//         this.roiService = roiService;
-//     }
-
-//     @PostMapping("/code/{discountCodeId}")
-//     public ResponseEntity<RoiReport> generateReportForCode(@PathVariable Long discountCodeId) {
-//         return ResponseEntity.ok(roiService.generateReportForCode(discountCodeId));
-//     }
-
-//     @GetMapping("/{reportId}")
-//     public ResponseEntity<RoiReport> getReportById(@PathVariable Long reportId) {
-//         return ResponseEntity.ok(roiService.getReportById(reportId));
-//     }
-
-//     @GetMapping("/influencer/{influencerId}")
-//     public ResponseEntity<List<RoiReport>> getReportsForInfluencer(@PathVariable Long influencerId) {
-//         return ResponseEntity.ok(roiService.getReportsForInfluencer(influencerId));
-//     }
-// }
 
 
 
