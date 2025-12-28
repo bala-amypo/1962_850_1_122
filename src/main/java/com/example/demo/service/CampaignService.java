d82e2f4c8f597a08f7d91c977d53c71ddb5fc67b
bala-amypo <bala@amypo.in>
2025-12-28 17:57:28 +0000

    User commit

--- src/main/java/com/example/demo/service/CampaignService.java
+++ src/main/java/com/example/demo/service/CampaignService.java
@@ -15,18 +15,7 @@
 
 
 
-// package com.example.demo.service;
 
-// import com.example.demo.model.Campaign;
-// import java.util.List;
-
-// public interface CampaignService {
-//     Campaign updateCampaign(Long id, Campaign campaign);
-
-//     Campaign getCampaignById(Long id);
-
-//     List<Campaign> getAllCampaigns();
-// }
 
 package com.example.demo.service;
 
